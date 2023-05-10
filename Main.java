b6647a44d158e074cdc5dbf0ed5eb6d1da2571ea
Sinmysize <117319536+Sinmysize@users.noreply.github.com>
2023-05-09 21:54:25 -0700

    Updated Code

--- Main.java
+++ Main.java
@@ -1,46 +1,38 @@
+/* Biggest Thanks To Celvis For The Help :) */
+
 import java.io.File;
 import java.nio.file.Files;
 import javax.swing.JOptionPane;
-
 public class Main{
     public static void main(String[] args) {
-
         String path = "C:\\Program Files (x86)\\Roblox\\Versions";
         File reader = new File(path);
-
         String fileName = "ClientAppSettings.json";
-
         File[] contents = reader.listFiles();
-
-        for (int i = 0; i < 1; i++) {
-            if (contents[1].isDirectory()) {
-                String file = contents[1].getName();
-                String fileDir = path + "\\" + file + "\\" + "ClientSettings";
-
-                System.out.println(fileDir);
-
-                File folder = new File(fileDir);
-                File source = new File("Settings.json");
-                File settings = new File(fileDir + "\\" + fileName);
-
-                try {
-                    if (!folder.exists()) {
-                        if (folder.mkdirs()) {
-                            JOptionPane.showMessageDialog(null, "Client Settings Has Been Added To Roblox!");
-                            Files.copy(source.toPath(), settings.toPath());
-                        } else {
-                            JOptionPane.showMessageDialog(null, "Please Run As Administrator.");
-                        }
-                    } else {
-                        JOptionPane.showMessageDialog(null, "This File May Already Exist or The Roblox Version Is Outdated.");
-                    }
-        
-                } catch (Exception e) {
-                    e.printStackTrace();
+        File mRD = contents[0];
+        for (File file : contents) {
+            if (file.isDirectory() && file.lastModified() > mRD.lastModified()) {
+                mRD = file;
+            }
+        }
+        String dirName = mRD.getName();
+        String fileDir = path + "\\" + dirName + "\\" + "ClientSettings";
+        File folder = new File(fileDir);
+        File source = new File("Settings.json");
+        File settings = new File(fileDir + "\\" + fileName);
+        try {
+            if (!folder.exists()) {
+                if (folder.mkdirs()) {
+                    JOptionPane.showMessageDialog(null, "Client Settings Has Been Added To Roblox!");
+                    Files.copy(source.toPath(), settings.toPath());
+                } else {
+                    JOptionPane.showMessageDialog(null, "Please Run As Administrator.");
                 }
             } else {
-                JOptionPane.showMessageDialog(null, "Sorry. Could Not Find Latest Roblox Version.");
-            }
-        }       
+                JOptionPane.showMessageDialog(null, "This File May Already Exist or Roblox Has Updated.");
+            }    
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
